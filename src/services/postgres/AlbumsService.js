732f3a49b09b1e4980d53ef2b6f82ac6557c66e2
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-03-16 15:33:21 +0700

    cleaning code

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -37,7 +37,7 @@ class AlbumsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('Album tidak ditemukan');
     }
 
